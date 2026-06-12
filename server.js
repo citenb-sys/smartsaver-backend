c8b00a53314f754eae47001f0fba6a67c1b9aa3a
nono Belaredj <nh.belaredj@web.de>
2026-06-12 20:34:25 +0200

    Use Firebase env variable

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const translate = require("translate-google");
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 initializeApp({
   credential: cert(serviceAccount),
